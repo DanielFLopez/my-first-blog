6a41a70504ab6e4c8afa7fc139cf90ec4b2006c2
DanielFLopez <daniel.f.lopez.j@gmail.com>
2016-12-06 18:01:25 -0500

    cambios push

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xy95n=+ce*n^c1(h8&)z0mfx91=4&emruk#m0wl#q-#aigz!ad'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['danielflj.pythonanywhere.com']
+ALLOWED_HOSTS = ['danielflj.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
